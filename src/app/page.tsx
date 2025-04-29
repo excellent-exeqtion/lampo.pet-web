63b519213f729fb22e301306edc6633f01dd67d2
Johnnatan <j.ruiz@exe.com.co>
2025-04-29 18:47:23 -0500

    fixing calendar page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import "@picocss/pico";
-import CalendarModule from "./pages/calendar/page.tsx";
+import CalendarModule from "./pages/calendar/page";
 
 export default function Home() {
   return (

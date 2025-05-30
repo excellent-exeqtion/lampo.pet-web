49a06e735398038cd91444bdf0a72e23df8d91a9
Johnnatan <excellent.exeqtion@gmail.com>
2025-05-30 05:19:12 -0500

    adding vercel token

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,8 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_ENABLE_ENCRYPTION: process.env.NEXT_PUBLIC_ENABLE_ENCRYPTION,
-    NEXT_PUBLIC_STORAGE_SECRET: process.env.NEXT_PUBLIC_STORAGE_SECRET
+    NEXT_PUBLIC_STORAGE_SECRET: process.env.NEXT_PUBLIC_STORAGE_SECRET,
+    VERCEL_ACCESS_TOKEN: process.env.VERCEL_ACCESS_TOKEN
   },
   async headers() {
     return [
@@ -27,6 +28,7 @@ const nextConfig: NextConfig = {
                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                { key: "Authorization", value: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` },
             ]
         }
     ]

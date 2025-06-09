311ae17d2f8b30c4315987d07929949e40b6c526
Johnnatan <excellent.exeqtion@gmail.com>
2025-06-08 19:35:10 -0500

    fixing vercel erros

--- src/app/landing/components/Navbar.tsx
+++ src/app/landing/components/Navbar.tsx
@@ -1,10 +1,8 @@
 // /app/landing/components/Navbar.tsx
-import { useDeviceDetect } from "@/hooks/useDeviceDetect";
 import styles from "../landing.module.css";
 import Image from "next/image";
 
 export default function Navbar() {
-    const { isMobile } = useDeviceDetect();
     return (
         <header className={styles.header_section}>
             <nav className={styles.navbar}>
@@ -12,7 +10,7 @@ export default function Navbar() {
                     <Image src="/images/logo.png" width={40} height={40} alt="Lampo Logo" />
                     <span>Lampo</span>
                 </div>
-                <ul className={styles.nav_links} style={{ display: isMobile ? 'block' : 'flex' }}>
+                <ul className={styles.nav_links}>
                     <li><a href="#about">¿Qué es Lampo?</a></li>
                     <li><a href="#features">Beneficios</a></li>
                     <li><a href="#category">Categorias</a></li>

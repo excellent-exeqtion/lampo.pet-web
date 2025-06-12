888fd5acdbfbf10c45faea5d4750258ea0bb93dc
Johnnatan <excellent.exeqtion@gmail.com>
2025-06-11 19:33:25 -0500

    Trying to fix vercel issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  pageExtensions: [
+        "page.tsx",
+        "page.ts",
+        "ts"
+    ],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],

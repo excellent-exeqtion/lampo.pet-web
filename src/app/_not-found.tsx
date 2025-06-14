6ffc6cbfff48a7cb6ec473f83602bffc4f2da56b
Johnnatan <excellent.exeqtion@gmail.com>
2025-06-14 17:25:17 -0500

    maybe?

--- src/app/_not-found.tsx
+++
@@ -1,9 +0,0 @@
-// app/_not-found.tsx
-export default function NotFound() {
-    return (
-        <main style={{ textAlign: "center", padding: "2rem" }}>
-            <h1>Página no encontrada</h1>
-            <p>Lo sentimos, la página que buscas no existe.</p>
-        </main>
-    );
-}

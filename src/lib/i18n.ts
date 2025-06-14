e3415609366d2df121b14bb57678977b3e349e8f
Johnnatan <excellent.exeqtion@gmail.com>
2025-06-14 15:48:49 -0500

    trying to fix i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,18 +1,21 @@
-// src/i18n.ts
+// src/lib/i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import es from '../../public/locales/es/errors.json';
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources: {
-            es: { errors: es },
-        },
-        lng: 'es',
-        fallbackLng: 'es',
-        interpolation: { escapeValue: false },
-    });
+// Comprobamos si estamos en el navegador antes de inicializar
+if (typeof window !== 'undefined' && !i18n.isInitialized) {
+    i18n
+        .use(initReactI18next)
+        .init({
+            resources: {
+                es: { errors: es },
+            },
+            lng: 'es',
+            fallbackLng: 'es',
+            interpolation: { escapeValue: false },
+        });
+}
 
 export default i18n;
